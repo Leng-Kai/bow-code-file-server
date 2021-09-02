032dae189a00983ffd19ac40d4a5493e953f813c
Leng-Kai <b07705049@ntu.edu.tw>
2021-09-02 14:21:44 +0800

    Add CORS allow methods

--- src/pkg/download/download.go
+++ src/pkg/download/download.go
@@ -35,7 +35,7 @@ func Handler2HandlerFunc(handler http.Handler) http.HandlerFunc {
         w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
         w.Header().Set("Access-Control-Allow-Origin", "http://api.ramen-live.com:3000")
         w.Header().Set("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
-        w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
 
         handler.ServeHTTP(w, r)
